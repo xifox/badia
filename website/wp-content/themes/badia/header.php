0263add614e4b13e9fe3fae8ddc85f8c32897aba
Laura <lauramelos@gmail.com>
2012-08-28 16:35:25 -0300

    modificaciones varias

--- website/wp-content/themes/badia/header.php
+++ website/wp-content/themes/badia/header.php
@@ -131,13 +131,21 @@
 
 <div id="slides">
         <div class="slides_container">
-          <a href="http://www.flickr.com/photos/jliba/4665625073/" title="145.365 - Happy Bokeh Thursday! | Flickr - Photo Sharing!" target="_blank"><img src="http://slidesjs.com/examples/standard/img/slide-1.jpg" width="570" height="270" alt="Slide 1"></a>
-          <a href="http://www.flickr.com/photos/stephangeyer/3020487807/" title="Taxi | Flickr - Photo Sharing!" target="_blank"><img src="http://slidesjs.com/examples/standard/img/slide-2.jpg" width="570" height="270" alt="Slide 2"></a>
-          <a href="http://www.flickr.com/photos/childofwar/2984345060/" title="Happy Bokeh raining Day | Flickr - Photo Sharing!" target="_blank"><img src="http://slidesjs.com/examples/standard/img/slide-3.jpg" width="570" height="270" alt="Slide 3"></a>
-          <a href="http://www.flickr.com/photos/b-tal/117037943/" title="We Eat Light | Flickr - Photo Sharing!" target="_blank"><img src="http://slidesjs.com/examples/standard/img/slide-4.jpg" width="570" height="270" alt="Slide 4"></a>
-          <a href="http://www.flickr.com/photos/bu7amd/3447416780/" title="“I must go down to the sea again, to the lonely sea and the sky; and all I ask is a tall ship and a star to steer her by.” | Flickr - Photo Sharing!" target="_blank"><img src="http://slidesjs.com/examples/standard/img/slide-5.jpg" width="570" height="270" alt="Slide 5"></a>
-          <a href="http://www.flickr.com/photos/streetpreacher/2078765853/" title="twelve.inch | Flickr - Photo Sharing!" target="_blank"><img src="http://slidesjs.com/examples/standard/img/slide-6.jpg" width="570" height="270" alt="Slide 6"></a>
-          <a href="http://www.flickr.com/photos/aftab/3152515428/" title="Save my love for loneliness | Flickr - Photo Sharing!" target="_blank"><img src="http://slidesjs.com/examples/standard/img/slide-7.jpg" width="570" height="270" alt="Slide 7"></a>
+
+<?php
+  $slideID = 33; 
+  $slides = get_children(array('post_type' => 'attachment', 'post_parent' => $slideID, 'orderby' => 'menu_order','order' => 'ASC')); ?>
+        <?php if ($slides == true) foreach($slides as $id => $attachment) : ?>
+
+          <?php
+            $img = wp_get_attachment_image_src($id, 'full');
+            $imgth = wp_get_attachment_image_src($id, 'full');
+
+                        ?>
+            <img data-original="<?php echo $img[0] ?>" class="lazy-slide" src="<?php echo $imgth[0]  ?>" width="<?php echo $img[1] ?>"  height="<?php echo $img[2] ?>"  alt="<?php $post->post_title ?>" />
+
+        <?php endforeach; ?>
+
         </div>
         <a href="#" class="prev"><img src="<?php echo get_stylesheet_directory_uri(); ?>/Slides/examples/Standard/img/arrow-prev.png" width="24" height="43" alt="Arrow Prev"></a>
         <a href="#" class="next"><img src="<?php echo get_stylesheet_directory_uri(); ?>/Slides/examples/Standard/img/arrow-next.png" width="24" height="43" alt="Arrow Next"></a>

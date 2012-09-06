f73293135c2b87153d20ef6e52debdb4f1f7cae0
Alex <alex.m3lo@gmail.com>
2012-09-06 16:01:37 -0300

    modificamos estilos

--- website/wp-content/themes/badia/header.php
+++ website/wp-content/themes/badia/header.php
@@ -48,6 +48,7 @@
 <link rel="profile" href="http://gmpg.org/xfn/11" />
 <link rel="stylesheet" type="text/css" media="all" href="<?php bloginfo( 'stylesheet_url' ); ?>" />
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
+<link href='http://fonts.googleapis.com/css?family=Oswald:400,700,300' rel='stylesheet' type='text/css'>
 <!--[if lt IE 9]>
 <script src="<?php echo get_template_directory_uri(); ?>/js/html5.js" type="text/javascript"></script>
 <![endif]-->
